ea5f1e347601dc76feced066a9e60484052de6cd
filip-daca <filip.hubert.daca@gmail.com>
2021-01-09 17:14:23 +0100

    Monitoring controller joy directions

--- examples/ControllerMonitoring/controller_monitoring/controller_monitoring.ino
+++ examples/ControllerMonitoring/controller_monitoring/controller_monitoring.ino
@@ -12,11 +12,15 @@ void setup() {
 void loop() {
   gb.update();
 
-  Serial.println("A \t B \t X \t Y");
+  Serial.println("A \t B \t X \t Y \t L \t R \t U \t D");
   Serial.println(String(gb.controller.a) + '\t' +
     String(gb.controller.b) + '\t' +
     String(gb.controller.x) + '\t' +
-    String(gb.controller.y) + '\t'
+    String(gb.controller.y) + '\t' +
+    String(gb.controller.left) + '\t' +
+    String(gb.controller.right) + '\t' +
+    String(gb.controller.up) + '\t' +
+    String(gb.controller.down) + '\t'
   );
   
   delay(DELAY);
